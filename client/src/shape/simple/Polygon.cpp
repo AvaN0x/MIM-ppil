750b7b8f8f9f09780584dd305b0992b9e3547014
Fentomee <71125325+LucBrungard@users.noreply.github.com>
2022-02-05 22:23:43 +0100

    Add Polygon Shape

---
+++ client/src/shape/simple/Polygon.cpp
@@ -0,0 +1,65 @@
+#include "Polygon.h"
+
+Polygon::Polygon(std::vector<Vecteur2D> points, Color color)
+{
+    if (points.size() < 3)
+        throw std::invalid_argument("You need at least 3 points to create a polygon");
+    _points = points;
+    _color = color;
+}
+
+Polygon::Polygon(const Polygon &shape)
+{
+    _points = shape.getPoints();
+    _color = shape.getColor();
+}
+
+Polygon::Polygon(const std::string &str)
+{
+    // TODO @Avan0x
+}
+
+Polygon::operator std::string() const
+{
+    // TODO
+}
+
+Shape *Polygon::homothety(const Vecteur2D &origin, double coeff) const
+{
+    std::vector<Vecteur2D> points;
+    for (const Vecteur2D &point : _points)
+    {
+        points.push_back(point * coeff + origin * (1 - coeff));
+    }
+    return new Polygon(points, _color);
+}
+
+Shape *Polygon::translation(const Vecteur2D &v) const
+{
+    std::vector<Vecteur2D> points;
+    for (const Vecteur2D &point : _points)
+    {
+        points.push_back(point + v);
+    }
+    return new Polygon(points, _color);
+}
+
+Shape *Polygon::rotation(const Vecteur2D &origin, double alpha) const
+{
+    // TODO
+}
+
+double Polygon::area() const
+{
+    // TODO
+}
+
+bool Polygon::operator==(Shape *shape) const
+{
+    return SimpleShape::operator==(shape);
+}
+
+bool Polygon::operator!=(Shape *shape) const
+{
+    return !(*this == shape);
+}
--- client/src/shape/simple/Polygon.h
+++ client/src/shape/simple/Polygon.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include "SimpleShape.h"
+
+class Polygon : public SimpleShape
+{
+    // ==================== CONSTRUCTORS ====================
+    Polygon(std::vector<Vecteur2D> points, Color color);
+    Polygon(const Polygon &shape);
+    Polygon(const std::string &str);
+
+    // ==================== CLONE ====================
+    Shape *clone() const { return new Polygon(*this); }
+
+    // ==================== STRING ====================
+    operator std::string() const;
+
+    // ==================== OPERATIONS ====================
+    Shape *homothety(const Vecteur2D &origin, double coeff) const;
+    Shape *translation(const Vecteur2D &v) const;
+    Shape *rotation(const Vecteur2D &origin, double alpha) const;
+    double area() const;
+
+    // ==================== OPERATORS ====================
+    bool operator==(Shape *shape) const;
+    bool operator!=(Shape *shape) const;
+};
